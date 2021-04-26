cfefdfcb3d4007e2249f61a799b02758c910d4b6
Klecko <david.mateos.romero@gmail.com>
2021-04-26 21:51:55 +0200

    Script for generating basic blocks now requires ELF base address

--- scripts/generate_basic_blocks.py
+++ scripts/generate_basic_blocks.py
@@ -4,15 +4,20 @@ import time
 import sys
 
 def main():
-	if len(sys.argv) < 3:
-		print("usage: %s elf_path output_path" % sys.argv[0])
+	if len(sys.argv) < 4:
+		print("usage: %s elf_path output_path base_address_hex" % sys.argv[0])
 		sys.exit(-1)
 
 	elf_path = sys.argv[1]
 	output_path = sys.argv[2]
+	base_address = int(sys.argv[3], 16)
 	with open(output_path, "w") as f:
 		print("Creating file '%s' from '%s'" % (output_path, elf_path))
-		project = angr.Project(elf_path, load_options={'auto_load_libs': False})
+		project = angr.Project(
+			elf_path,
+			load_options={'auto_load_libs': False},
+			main_opts={'custom_base_addr': base_address}
+		)
 
 		t = time.time()
 		cfg = project.analyses.CFGFast()
