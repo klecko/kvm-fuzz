829e006fcf6f8dcc1ee6a442dc31db9b6a1b6d06
Klecko <david.mateos.romero@gmail.com>
2021-05-20 11:57:48 +0200

    Kernel: LTO and rewrite SafeMem in naked assembly

--- kernel/src/libcpp/safe_mem.cpp
+++ kernel/src/libcpp/safe_mem.cpp
@@ -7,42 +7,36 @@ extern "C" void* safe_memcpy_ins_faulted;
 extern "C" void* safe_strlen_ins_may_fault;
 extern "C" void* safe_strlen_ins_faulted;
 
+__attribute__((naked))
 bool memcpy(void* dest, const void* src, size_t n) {
 	asm volatile(
+		"mov rcx, rdx;"              // move n to rcx
 		"safe_memcpy_ins_may_fault:"
-		"rep movsb;"
+		"rep movsb;"                 // mov from rsi (src) to rdi (dest)
 		"safe_memcpy_ins_faulted:"
-		: "=c" (n)    // move rcx to `n` as result
-		: "D" (dest), // rdi
-		  "S" (src),  // rsi
-		  "c" (n)     // rcx
-		: "memory"
+		"test rcx, rcx;"             // if rcx is not 0, it's because we faulted
+		"sete al;"                   // so just return wheter rcx is 0 or not
+		"ret;"
 	);
-	if (n > 0) {
-		// The 'rep movsb' faulted and the operation wasn't completed
-		return false;
-	}
-	return true;
 }
 
+__attribute__((naked))
 ssize_t strlen(const char* s) {
-	ssize_t n;
 	asm volatile(
+		"xor rax, rax;" // look for null byte
+		"xor rcx, rcx;"
+		"dec rcx;"      // set rcx to -1
 		"safe_strlen_ins_may_fault:"
-		"repne scasb;"
+		"repne scasb;"  // scan string until we find a null byte
 		"not rcx;"
-		"dec rcx;"
 		"jmp end;"
 		"safe_strlen_ins_faulted:"
-		"xor ecx, ecx;" // set rcx to -1
-		"dec rcx;"
+		"xor ecx, ecx;" // we faulted: set rcx to -1
 		"end:"
-		: "=c" (n)    // move rcx to `n` as result
-		: "a" (0),    // rax, value we're looking for in the string
-		  "c" (-1LL), // rcx, max length
-		  "D" (s)     // rdi, string address
+		"dec rcx;"
+		"mov rax, rcx;"
+		"ret;"
 	);
-	return n;
 }
 
 bool handle_safe_access_fault(InterruptFrame* frame) {
